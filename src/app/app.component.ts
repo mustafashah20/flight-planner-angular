7a6e91e8384b48e87f277bed5f0bce3941318e37
Mustafa <mustafashah0000@gmail.com>
2021-08-12 00:20:14 +0500

    title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'flight-planner-angular';
+  title: String = 'Flight Planner';
 }
