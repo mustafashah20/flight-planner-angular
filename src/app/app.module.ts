549980ada5daeb9e231e8a7bec2cb98dc9ca02ef
Mustafa Shah <i170253@nu.edu.pk>
2021-08-12 11:31:00 +0500

    admin-home added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminHomeComponent
   ],
   imports: [
     BrowserModule,
